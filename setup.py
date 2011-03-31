c100c886f479c5964b37737b90aa83dda33f7cca
Alex Kalderimis <alex.kalderimis@gmail.com>
2011-03-31 23:38:29 +0100

    added test and clean commands to setup

--- setup.py
+++ setup.py
@@ -1,7 +1,62 @@
-from distutils.core import setup
+"""
+The test and clean code is shamelessly stolen from
+http://da44en.wordpress.com/2002/11/22/using-distutils/
+"""
+
+from distutils.core import Command, setup
+from unittest import TextTestRunner, TestLoader
+from glob import glob
+from os.path import splitext, basename, join as pjoin, walk
+import os
+
+class TestCommand(Command):
+    user_options = [ ]
+
+    def initialize_options(self):
+        self._dir = os.getcwd()
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        '''
+        Finds all the tests modules in tests/, and runs them.
+        '''
+        testfiles = [ ]
+        for t in glob(pjoin(self._dir, 'tests', '*.py')):
+            if not t.endswith('__init__.py'):
+                testfiles.append('.'.join(
+                    ['tests', splitext(basename(t))[0]])
+                )
+
+        tests = TestLoader().loadTestsFromNames(testfiles)
+        t = TextTestRunner(verbosity = 1)
+        t.run(tests)
+
+class CleanCommand(Command):
+    user_options = [ ]
+
+    def initialize_options(self):
+        self._clean_me = [ ]
+        for root, dirs, files in os.walk('.'):
+            for f in files:
+                if f.endswith('.pyc'):
+                    self._clean_me.append(pjoin(root, f))
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        for clean_me in self._clean_me:
+            try:
+                os.unlink(clean_me)
+            except:
+                pass
+
 setup(
         name = "intermine",
         packages = ["intermine"],
+        cmdclass = { 'test': TestCommand, 'clean': CleanCommand },
         version = "0.96.00",
         description = "InterMine WebService client",
         author = "Alex Kalderimis",
